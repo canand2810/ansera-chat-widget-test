1c45e32954e50c66427f7d7421688dae103f617e
laxbattini <lbattini@skywirenetworks.com>
2025-06-16 15:26:17 +0530

    production environment fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  define: {
+    'process.env.NODE_ENV': '"production"',
+  },
   build: {
     lib: {
       entry: 'src/main.jsx',
